59d5c6d2d4cc963cf2d47ef8b45619d9218b23cd
pdaszewski <piotr.daszewski@fxsystems.com.pl>
2020-03-02 06:16:23 +0100

    code version 0.7.0.0

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ A tool for the visual design of a status flow diagram.
 Useful, for example, for CRM or ERP systems.
 
 Some screen:
-![alt text](https://github.com/pdaszewski/Diagram-Designer/blob/master/Screen.png)
+![alt text](https://github.com/pdaszewski/Diagram-Designer/blob/master/Screens/Screen.png)
 
 This tool has new project writen in Delphi FMX multidevice technology for Windows, Android and iOS:
 https://github.com/pdaszewski/FMX-Diagram-Designer
---
+++ StatusDiagramDesigner.dpr
@@ -0,0 +1,24 @@
+﻿{
+Cały projekt musi trafić do przebudowy.
+Jest nowy z 2018 roku i warto zastosować w nim dobre praktyki projektowania i czystego kodu.
+}
+
+program StatusDiagramDesigner;
+
+uses
+  Vcl.Forms,
+  DiagramDev_frm in 'DiagramDev_frm.pas' {DiagramDev},
+  DiagramPowiazanie_frm in 'DiagramPowiazanie_frm.pas' {DiagramPowiazanie},
+  Vcl.Themes,
+  Vcl.Styles;
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.Title := 'Projektowanie diagramu statusów';
+  Application.CreateForm(TDiagramDev, DiagramDev);
+  Application.CreateForm(TDiagramPowiazanie, DiagramPowiazanie);
+  Application.Run;
+end.
